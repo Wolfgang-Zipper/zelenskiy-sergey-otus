85b4b59a78b5e1381f73a225dc1dec1505f0d2f2
Wolfgang-Zipper <sergzelenscky@yandex.ru>
2023-07-11 00:21:47 +0300

    подсчет повторений товара, сортировка

--- javascrypt-2/index.js
+++ javascrypt-2/index.js
@@ -1,31 +1,51 @@
-let shopList = [["a", "b"], ["a", "c"], ["d", "e"]]
-let association = [];
+let shopList = [
+    ["q", "w", 'a'],
+    ["a", "b"],
+    ["a", "c"],
+    ["q", "e"],
+    ["q", "r"],
+]
 
-function maxItemAssociation() {
-shopList.forEach(products => { //перебираем список покупок
+let association = {};
+let associationMax = [];
 
-    products.forEach(productsItem => { //перебираем покупку
- 
-        // if (!association.includes(productsItem)) { //проверяем, содержится ли товар в ассоциациях
-        //     console.log(`${products} содержит ${productsItem}`) 
+function maxItemAssociation(shop) {
 
-        //     association.push(productsItem)
-        // }
+    shop.forEach(products => { //перебираем список покупок
 
-        if (products.includes(productsItem)) { //проверяем, содержится ли товар в покупке
-            console.log(`${products} содержит ${productsItem}`) 
+        products.forEach(productsItem => { //перебираем покупку
 
-            association.push(productsItem)
-        }
+            if (association.hasOwnProperty(productsItem)) { // подсчитываем количество повторений товара, тем самым получаем товар, встречающийся в максимальных рекоментациях
+                association[productsItem] += 1
+            } else {
+                association[productsItem] = 1
+                
+            }
 
-
+        });
     });
 
-});
+    for (let item of Object.keys(association)) { //перебираем обьект товаров с количеством
 
-return association;
+        if (association[item] >= 2) { // если количество всетрается 2 раза и больше, то
+
+            shop.forEach(products => { //перебираем список покупок
+
+                products.forEach(productsItem => { //перебираем покупку
+
+                    if (products.some(e => e == item) && !associationMax.includes(productsItem)) {
+
+                        associationMax.push(productsItem) // если покупка содержит товар, встречающийся в максимальных рекоментациях, добавляем в обьект 
+
+                    }
+                });
+
+            });
+
+        }
+    }
+
+    return associationMax.sort(); // сортируем
 }
 
-console.log(maxItemAssociation(shopList))
-
-
+console.log(maxItemAssociation(shopList)) // ['a', 'b', 'c', 'e', 'q', 'r', 'w']
