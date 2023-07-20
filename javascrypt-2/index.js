96751714d78049356f8d36c5a7b25f0b1a6b9e6e
Sergey <s.zelenskiy@cloudpayments.ru>
2023-07-20 06:26:31 +0300

    прибавляем каждый результат reduce к result

--- javascrypt-2/index.js
+++ javascrypt-2/index.js
@@ -1,6 +1,6 @@
 var fn1 = () => {
   console.log('fn1')
-  return Promise.resolve(1)
+  return Promise.resolve(5) //добавлено 5 для проверки
 }
 
 var fn2 = () => new Promise(resolve => {
@@ -13,7 +13,8 @@ async function promiseReduce (asyncFunctions, reduce, initialValue) {
   let result = 0; 
   for (func of asyncFunctions) { //перебираем массив функций, forEach не подошел, так как не возвращает промисы
       await func().then(res => { //ждем выпонления функции в цикле, после выполнения (.then), значение, возвращаемое функциями в массиве, обрабатываем в функции reduce и назначем переменной result, которую отдает promiseReduce
-        result = reduce(res, initialValue)
+        result += reduce(res, initialValue) //прибавляем каждый результат reduce к result
+
       })
   }
 
@@ -23,7 +24,8 @@ async function promiseReduce (asyncFunctions, reduce, initialValue) {
 
 promiseReduce([fn1, fn2],
   function (memo, value) {
-      console.log('reduce')
+      console.log('reduce') 
+      console.log(memo * value) //лог для проверки вызхова функции для каждого успешно завершившегося промиса
 
       return memo * value
   },
